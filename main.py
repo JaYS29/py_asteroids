635ebff593cf000b5b305e2e1dfd59b592d34903
Juan Jose Churruarin <juangnr@gmail.com>
2024-09-18 17:52:15 -0300

    Complete the rest of the game files

--- main.py
+++ main.py
@@ -1,21 +1,51 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
 import pygame
+
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
+from player import Player
+from shot import Shot
+
 
 def main():
-  print("Starting asteroids!")
-  print(f"Screen width: {SCREEN_WIDTH}")
-  print(f"Screen height: {SCREEN_HEIGHT}")
-  
-  pygame.init()
-  
-  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-  
-  while True:
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        return
-    screen.fill('black',rect=None, special_flags=0)
-    pygame.display.flip()
-    
+    pygame.init()
+    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = updatable
+    Shot.containers = (updatable, drawable, shots)
+    # create Player instance AFTER adding Player to the groups
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    asteroidField = AsteroidField()
+    while True:
+        dt = clock.tick(60) / 1000
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(color=(0, 0, 0))
+        for sprite in updatable:
+            sprite.update(dt)
+        for sprite in asteroids:
+            if sprite.check_collision(player):
+                raise SystemExit("Game Over!")
+        for sprite in asteroids:
+            for shot in shots:
+                if shot.check_collision(sprite):
+                    sprite.split()
+                    shot.kill()
+        for sprite in drawable:
+            sprite.draw(screen)
+        pygame.display.flip()
+
+
 if __name__ == "__main__":
-  main()
+    main()
